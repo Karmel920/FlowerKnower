07d9027a66c982866d39c978863577b043696e79
vpukas <valera.pukas14@gmail.com>
2023-03-21 00:21:45 +0100

    Added basic image upload handling

---
+++ backend/src/main/java/com/flowerknower/backend/model/entities/Image.java
@@ -0,0 +1,21 @@
+package com.flowerknower.backend.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    private String imagePath;
+}
---
+++ backend/src/main/java/com/flowerknower/backend/services/ImageService.java
@@ -0,0 +1,40 @@
+package com.flowerknower.backend.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.flowerknower.backend.model.entities.Image;
+import com.flowerknower.backend.repositories.ImageRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final ImageRepository imageRepository;
+    @Value("${img.path}")
+    private String PATH;
+
+    public Image uploadImage(MultipartFile file) throws IOException {
+        String fullPath=PATH+file.getOriginalFilename();
+        Image image = Image.builder()
+                            .name(file.getOriginalFilename())
+                            .type(file.getContentType())
+                            .imagePath(fullPath)
+                            .build();
+        file.transferTo(new File(fullPath));
+        return imageRepository.save(image);
+    }
+
+    public byte[] downloadImage(String fileName) throws IOException{
+        Optional<Image> image = imageRepository.findByName(fileName);
+        String fullPath = image.get().getImagePath();
+        return Files.readAllBytes(new File(fullPath).toPath());
+    }
+}
