f34101b6325613f22811001cc6f004e1ed528f3f
matows <mateusz.owsiak@student.pk.edu.pl>
2023-04-03 09:03:56 +0200

    Added new dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
-
+import "leaflet/dist/leaflet.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
         <React.StrictMode>
