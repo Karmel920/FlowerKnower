0511ef4e01af6f88b2d2b9d684bcfaa00ca4b809
matows <mateusz.owsiak@student.pk.edu.pl>
2023-04-03 09:05:16 +0200

    Updated view for identified plant(added map)

--- frontend/src/views/identify/Identify.js
+++ frontend/src/views/identify/Identify.js
@@ -4,12 +4,32 @@ import Header from "../../public/components/Header";
 import Navbar from "../../public/components/Navbar";
 import { useLocation, Link } from 'react-router-dom';
 import AddLocationIcon from '@mui/icons-material/AddLocation';
-import { Button } from '@mui/material';
-
+import { Button, Tooltip } from '@mui/material';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import CloseIcon from '@mui/icons-material/Close';
+import {IconButton} from '@mui/material';
 function Identify(){
 
     const location = useLocation();
     const {img} = location.state;
+
+    const [mapLocation, setMapLocation] = React.useState(null);
+    const [showMapPopup, setShowMapPopup] = React.useState(false);
+
+    const handleMapClick = (event) => {
+        // const {latlng} = event;
+        // setMapLocation(latlng);
+        console.log("Klikłem");
+    }
+
+    const handleAddLocalizationClick = () => {
+        setShowMapPopup(true);
+    }
+
+    const handleCloseMapPopup = () => {
+        setShowMapPopup(false);
+    }
+    
     return(
         <div className={styles.container}>
             <header style={{verticalAlign:"top"}}><Header title={"Identify plant"}/></header>
@@ -20,9 +40,38 @@ function Identify(){
                     <div className={styles.plantInfoContainer}>
                         <div className={styles.plantInfoHeader}>
                             <p className={styles.text}>You found: Daisy</p>
-                            <Button className={styles.locationButton} variant="outlined" endIcon={<AddLocationIcon/>}>
+                            <Button className={styles.locationButton} variant="outlined" endIcon={<AddLocationIcon/>} onClick={handleAddLocalizationClick}>
                                 Add discovery location
                             </Button>
+
+                            {showMapPopup && (
+                                <div className = {styles.mapPopup}>
+                                    <Tooltip title="Close map">
+                                        <IconButton onClick={handleCloseMapPopup} sx={{position:"fixed",alignSelf:"flex-end", zIndex:2}}>
+                                            <CloseIcon/>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <MapContainer
+                                        center={[50.06540687121868, 19.939021772031865]}
+                                        zoom = {13}
+                                        onClick={handleMapClick}
+                                        style={{width:'100%', height:"100%", zIndex:1}}
+                                    >
+                                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
+
+                                            {mapLocation && (
+                                                <Marker position={mapLocation}>
+                                                    <Popup>
+                                                        <span>You added marker at: {mapLocation.lat}, {mapLocation.lng}</span>
+                                                    </Popup>
+                                                </Marker>
+                                            )}
+                                    </MapContainer>
+                                </div>
+                            )}
+
+
+
                         </div>
                         <div className={styles.plantInfoDescription}>
                             <p className={styles.descriptionText}>Bellis perennis - the daisy, is a European species of the family Asteraceae, often considered the archetypal species of the name daisy. To distinguish this species from other plants known as daisies, it is sometimes qualified as common daisy, lawn daisy or English daisy.</p>
