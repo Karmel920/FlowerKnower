1aad2a61ad2d35697dafd0f5a440e43c698d60ee
matows <mateusz.owsiak@student.pk.edu.pl>
2023-04-08 13:27:59 +0200

    Added handling user registration and login

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
 import MainLogo from '../../public/img/main_logo.png';
 import SideLogo from '../../public/img/side_logo.svg';
-import { Link }  from 'react-router-dom';
+import { Link, useNavigate }  from 'react-router-dom';
 import styles from '../../public/modules/login.module.css';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import {Box} from "@mui/system";
 
 function Login(){
+
+    const navigate = useNavigate();
+    const [email, setEmail] = React.useState("");
+    const [password,setPassword] = React.useState("");
+    const [validationErrors, setValidationErrors] = React.useState({});
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
+    const [spinner, setSpinner] = React.useState(false);
+    const handleEmailChange = (event) =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    React.useEffect(()=>{
+        if(localStorage.getItem('token') !== '' && localStorage.getItem('token') != null){
+            navigate('/');
+        }
+    },[])
+
+    const handleSubmit = (e) => {
+        setSpinner(true);
+        setValidationErrors({});
+        e.preventDefault();
+        setIsSubmitting(true);
+        const data = {
+            email:email,
+            password:password
+        };
+        axios.post('http://localhost:8080/api/v1/auth/authenticate',data, {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then(response => {
+                
+                setSpinner(false);
+                setIsSubmitting(false);
+                localStorage.setItem('token', response.data.token);
+                navigate("/");
+            })
+            .catch(error =>{
+                setSpinner(false);
+                setIsSubmitting(false);
+                // console.log(error.response.status);
+                // if(error.response.status !== undefined){
+                    setValidationErrors([{message:"Wrong email or password"}]);
+                // }
+            });
+    }
+
 
     return(
         <div className={styles.main}>
@@ -12,10 +66,14 @@ function Login(){
                 <div className={styles.loginPanel}>
                     <img id={styles.mainLogo} src={MainLogo} alt="Main logo"/>
                     <p className={styles.mainText}>Login to Your Account</p>
-                    <form className={styles.loginForm}>
-                        <input id={styles.emailInput} placeholder="Email" type="email" name="email"/>
-                        <input id={styles.passwordInput} placeholder="Password" type="password" name="password"/>
-                        <button id={styles.loginButton}  type="submit">Sign in</button>
+                    <form className={styles.loginForm} onSubmit={handleSubmit}>
+                        {Object.keys(validationErrors).length !==0 &&
+                            <p className={styles.errorText}>Wrong email address or password</p>
+                        }
+                        <input id={styles.emailInput} placeholder="Email" type="email" onChange={handleEmailChange}/>
+                        <input id={styles.passwordInput} placeholder="Password" type="password" onChange={handlePasswordChange} />
+                        {spinner && (<Box sx={{mt:"2ch", display:"flex", justifyContent:"center"}}><CircularProgress/></Box>)}
+                        <button id={styles.loginButton} type="submit" disabled={isSubmitting}>Sign in</button>
                     </form>
                 </div>
                 <div className={styles.registerPanel}>
--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -3,8 +3,82 @@ import styles from '../../public/modules/register.module.css';
 import MainLogo from '../../public/img/main_logo.png';
 import SideLogo from '../../public/img/side_logo.svg';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+function check_password(password){
+    var upperCase = /[A-Z]/g;
+    var lowerCase = /[a-z]/g;
+    var number = /[0-9]/g;
+    if(password.match(upperCase) && password.match(lowerCase) && password.match(number))
+        return true;
+    return false;
+}
 
 function Register(){
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [repeatPassword, setRepeatPassword] = React.useState('');
+    const [validationErrors, setValidationErrors] = React.useState([]);
+    const [validationSuccess, setValidationSuccess] = React.useState([]);
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
+    const [spinner, setSpinner] = React.useState(false);
+
+    const handleEmailChange = (event) =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+    const handleRepeatPasswordChange = (event) => {
+        setRepeatPassword(event.target.value);
+    }
+
+    const handleSubmit = (e) =>{
+        setValidationErrors([]);
+        setValidationSuccess([]);
+        if(password.length < 8 || !check_password(password)){
+            e.preventDefault();
+            setValidationErrors([{message:'Password must contain of 8 or more characters, it must containt one capital and small letter and one digit'}]);
+        }else{
+            if(password === repeatPassword){
+                 
+                setSpinner(true);
+                setValidationErrors([]);
+                e.preventDefault();
+                setIsSubmitting(true);
+                
+                const data = {
+                    email: email,
+                    password: password
+                };
+
+                axios.post('http://localhost:8080/api/v1/auth/register', data, {
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }).then(response=>{
+                    setSpinner(false);
+                    setIsSubmitting(false);
+                    if(response.data !== undefined){
+                        setValidationSuccess(response.data);
+                    }
+                }).catch(error=>{
+                    setSpinner(false);
+                    setIsSubmitting(false);
+                    if(error.response.data !== undefined){
+                        setValidationErrors(error.response.data);
+                        setValidationErrors([{message:"That email address is already in use"}]);
+                    }
+                });
+            }else{
+                e.preventDefault();
+                setValidationErrors([{message:"Passwords are not the same"}]);
+            }
+        }
+    }
 
     return(
         <div className={styles.main}>
@@ -12,11 +86,18 @@ function Register(){
                 <div className={styles.loginPanel}>
                     <img id={styles.logo} src={MainLogo} alt="Main logo"/>
                     <p className={styles.mainText}>Create an Account</p>
-                    <form className={styles.loginForm}>
-                        <input id={styles.emailInput} placeholder="Email" type="email" name="email"/>
-                        <input id={styles.passwordInput} placeholder="Password" type="password" name="password"/>
-                        <input id={styles.repeatPasswordInput} placeholder="Repeat password" type="password" name="repeat_password"/>
-                        <button id={styles.registerButton} type="submit">Sign up</button>
+                    <form className={styles.loginForm} onSubmit={handleSubmit}>
+                        {Object.keys(validationErrors).length !==0 &&
+                            <p className={styles.errorText}>{validationErrors[0].message}</p>
+                        }
+                        {Object.keys(validationSuccess).length !==0 &&
+                            <p className={styles.successText}>Registered successfully</p>
+                        }
+                        <input id={styles.emailInput} placeholder="Email" type="email" onChange={handleEmailChange}/>
+                        <input id={styles.passwordInput} placeholder="Password" type="password" onChange={handlePasswordChange}/>
+                        <input id={styles.repeatPasswordInput} placeholder="Repeat password" type="password" onChange={handleRepeatPasswordChange}/>
+                        {spinner && (<Box sx={{mt:"2ch", display:"flex", justifyContent:"center"}}><CircularProgress/></Box>)}
+                        <button id={styles.registerButton} type="submit" disabled={isSubmitting}>Sign up</button>
                     </form>
                 </div>
                 <div className={styles.registerPanel}>
