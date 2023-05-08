34382ad9394c65e4e34183cec417b29ee85b0ba0
matows <mateusz.owsiak@student.pk.edu.pl>
2023-05-08 16:58:36 +0200

    Fetching and displaying data about flower from server and

--- frontend/src/views/collection/Collection.js
+++ frontend/src/views/collection/Collection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from '../../public/modules/collection.module.css';
 import Header from "../../public/components/Header";
 import Navbar from "../../public/components/Navbar";
@@ -6,25 +6,82 @@ import Species from "./components/Species";
 import { Divider } from "@mui/material";
 import { Box } from "@mui/system";
 import { useNavigate } from "react-router";
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
 function Collection(){
+
     const navigate = useNavigate();
+
+    const [discoveries, setDiscoveries] = React.useState([]);
+    const [spinner, setSpinner] = React.useState(false);
+    const [openErrorSnackbar, setOpenErrorSnackbar] = React.useState(false);
+
+    useEffect(()=>{
+        if(localStorage.getItem('token') !== "" && localStorage.getItem('token') != null){
+            getDiscoveries();
+        }else{
+            navigate('/login');
+        }
+    },[]);
+
     const handleLogout = () => {
+        axios.get('http://localhost:8080/api/v1/auth/logout', {
+            headers:{
+                Authorization: 'Bearer ' + localStorage.getItem('token')
+            }
+        });
         localStorage.setItem('token',"");
+        localStorage.setItem('flowersCount',"");
         navigate("/login");
     }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        setOpenErrorSnackbar(false);
+      };
+    
+    const getDiscoveries = () => {
+        setSpinner(true);
+        axios.get('http://localhost:8080/api/v1/discovery', {
+            headers:{
+                Authorization: 'Bearer ' + localStorage.getItem('token')
+            }
+        }).then(response=>{
+            setSpinner(false);
+            setDiscoveries(response.data);
+        }).catch(error=>{
+            setSpinner(false);
+            setOpenErrorSnackbar(true);
+        });
+    }
+    
     return(
         <div className={styles.container}>
             <header style={{verticalAlign:"top"}}><Header title={"Your plants"} logoutAction={handleLogout}/></header>
             <div className={styles.main}>
                 <Navbar/>
                 <div className={styles.section}>
+                    <Snackbar anchorOrigin={{vertical: 'bottom', horizontal:'left'}} open={openErrorSnackbar} autoHideDuration={3000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                            Error while collecting data
+                        </Alert>
+                    </Snackbar>
                     <Box sx={{width:"100%"}}>
-                        <p className={styles.text}>2 species found</p>
+                        <p className={styles.text}>{discoveries.length} species found</p>
                         <Divider/>
                     </Box>
                     <div className={styles.speciesContainer}> 
-                        <Species name={"Daisy"} date={"13.03.2023"} description={"Bellis perennis"}/>
-                        <Species name={"Rose"} date={"27.03.2023"} description={''}/>
+                        {spinner && (<Box sx={{mt:"2ch", display:"flex", justifyContent:"center"}}><CircularProgress/></Box>)}
+                        {discoveries.map((discovery) => (
+                            <Species key={discovery.id} id={discovery.id} name={discovery.name} description={discovery.description} image={discovery.image} date={discovery.date} location={discovery.discoveryLocation}/>
+                        ))}
                     </div>
                 </div>
             </div>
