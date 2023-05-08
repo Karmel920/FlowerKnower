69f7b55d0671ee8d61b0f2fe2ef4a745b489e6e4
matows <mateusz.owsiak@student.pk.edu.pl>
2023-05-08 16:54:11 +0200

    Updated app info

--- frontend/src/views/info/Info.js
+++ frontend/src/views/info/Info.js
@@ -4,6 +4,8 @@ import Logo from '../../public/img/main_logo.png';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Divider, IconButton, Tooltip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import goodExample from './assets/good_example.jpg';
+import badExample from './assets/bad_example.jpg';
 function Info(){
     
     const navigate = useNavigate();
@@ -30,7 +32,21 @@ function Info(){
                         <Divider sx={{borderBottomWidth: 2}}/>
                         <br/>
                         <span>With our model you can classify the following flower species:</span>
-                        <span><b> viola, crocus, dandelion, forget-me-not, fern, snowdrop, rose, sunflower, bellis, orchid, tulip</b></span>
+                        <span><b> crocus, bellis, dandelion, pansy, rose, snowdrop, sunflower, tulip</b></span>
+                        <span>The photo you want to send to the model should contain only the flower, without unnecessary details.</span>
+                        <br/>
+                        <div className={styles.examples}>
+                            <div className={styles.example}>
+                                <span>Example of good photo: </span>
+                                <img id={styles.imgs} src={goodExample} alt="Example of good photo"/>
+                            </div>
+                            <div className={styles.example}>
+                                <span>Example of bad photo: </span>
+                                <img id={styles.imgs} src={badExample} alt="Example of bad photo"/>
+                            </div>
+                        </div>
+                        <br/>
+                        <span>Acceptable image extensions: JPG, jpg, jpeg, png</span>
                     </div>
                 </div>
             </div>
