745c4fd16149f7894f02ba8f6dd0bf86bc39c930
Eve0028 <sylwia14082@gmail.com>
2023-05-15 16:18:14 +0200

    Fix: ml axios

--- frontend/src/helpers/axios_ml.js
+++ frontend/src/helpers/axios_ml.js
@@ -4,10 +4,6 @@ const baseURL = process.env.REACT_APP_API_ML_BASE_URL;
 
 let headers = {};
 
-if(localStorage.token) {
-  headeers.Authorization = `Bearer ${localStorage.token}`;
-}
-
 const axiosInstance = axios.create({
   baseURL: baseURL,
   headers,
