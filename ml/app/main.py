811313a150f420fa685892f0c55b21b89f82e4e6
Eve0028 <sylwia14082@gmail.com>
2023-05-15 17:29:53 +0200

    Refactor: check all origins

--- ml/app/main.py
+++ ml/app/main.py
@@ -35,7 +35,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
